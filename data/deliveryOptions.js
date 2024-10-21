103a6e988c56c341aad142ba3b7fed2c691df4e9
ChanMyae <acm.electronic13@gmail.com>
2024-10-21 22:25:21 +0700

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions = [{
+  id: '1',
+  delievryDate: 7,
+  priceCents: 0
+}, {
+  id: '2',
+  delievryDate: 3,
+  priceCents: 499
+},{
+  id: '3',
+  delievryDate: 1,
+  priceCents: 999
+}];
