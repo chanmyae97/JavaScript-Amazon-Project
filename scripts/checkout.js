521ff4019a935a50854e1f022d019fa5e52797b8
ChanMyae <acm.electronic13@gmail.com>
2024-11-21 19:59:06 +0700

    Convert from promises Async and await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,23 +1,37 @@
-import {renderOrderSummary} from './checkout/orderSummary.js';
-import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {loadProductsFetch } from '../data/products.js';
-import { loadCart } from '../data/cart.js';
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { loadProductsFetch } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) =>{
-    loadCart(() =>{
+async function loadPage() {
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
       resolve();
     });
-  })
+  });
 
-]).then((values) =>{
-  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
-});
+}
+loadPage();
+
+// Promise.all([
+//   loadProductsFetch(),
+//   new Promise((resolve) =>{
+//     loadCart(() =>{
+//       resolve();
+//     });
+//   })
+
+// ]).then((values) =>{
+//   console.log(values);
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
 
 // new Promise((resolve ) =>{
 //   loadProducts(() =>{
